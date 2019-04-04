f6e08d83c083fece74bd83d946f0c1e1b36fc06d
Rvn0xsy <payloads@aliyun.com>
2019-04-04 11:55:02 +0800

    add ignore

--- server.py
+++ server.py
@@ -45,6 +45,7 @@ class Metasploit_RPC(BaseRequestHandler):
             cls(request, client_address, server, *args, **kwargs)
         return _HandlerCreator
 
+    # 请求API
     def _request(self,options):
         try:
             term.writeLine("[*]API URL : {url} , Method : {method}".format(url=self.url,method=options[0]), term.green)
@@ -57,16 +58,20 @@ class Metasploit_RPC(BaseRequestHandler):
                 return result
         except Exception as e:
             sys.stderr.write(str(e)+"\nRef:https://metasploit.help.rapid7.com/docs/standard-api-methods-referenc\n")
-
+    
+    # 获取Token
     def _get_token(self):
         options = ["auth.login",self.username,self.password]
         
         result = self._request(options)
         self.token = str(result[b'token'],encoding = "utf8")
         term.writeLine("[*]Token: {token} Username : {username} Password : {password}".format(token=self.token,username=self.username,password=self.password),term.green)
+    
+    # 打包数据
     def __pack(self,pack_str):
         return msgpack.packb(pack_str)
-
+    
+    # 解包数据
     def __unpack(self,pack_str):
         return msgpack.unpackb(pack_str)
 
@@ -81,27 +86,32 @@ class Metasploit_RPC(BaseRequestHandler):
         while True:
             data = self.request.recv(1024)
             if not data:break
-            data = data.decode()
-            data = json.loads(data)
-            payload = self.__send_payload(data)
-            term.writeLine("[*]PAYLOAD size: {size}".format(size=len(payload[b'payload'])),term.green)
-            self.request.send(payload[b'payload'])
+            try:
+                data = data.decode()
+                data = json.loads(data)
+                payload = self.__send_payload(data)
+                term.writeLine("[*]PAYLOAD size: {size}".format(size=len(payload[b'payload'])),term.green)
+                self.request.send(payload[b'payload'])
+            except Exception as e:
+                term.writeLine("[!]{error}".format(error=str(e)),term.red)
+            finally:
+                pass
 
 def main():
     example = 'Example:\n\n$ python3 server.py -U msf -P msf -v -s -l 4444'
     args = ArgumentParser(prog='Cooolis-ms',epilog=example)
     args.add_argument('-U','--username',help='Metasploit web service username',required=True)
     args.add_argument('-P','--password',help='Metasploit web service password',required=True)
-    args.add_argument('-H','--host',help='Metasploit web service host',default='localhost')
-    args.add_argument('-p','--port',help='Metasploit RPC service port',default=55553,type=int)
-    args.add_argument('-S','--server',help='Payload sender listen host',default='localhost')
-    args.add_argument('-l','--listen',help='Payload listen port',default=1111,type=int)
-    args.add_argument('-u','--uri',help='Metasploit RPC service uri',default='/api/1.0/')
+    args.add_argument('-H','--host',help='Metasploit web service host, Default: localhost',default='localhost')
+    args.add_argument('-p','--port',help='Metasploit RPC service port, Default: 55553',default=55553,type=int)
+    args.add_argument('-S','--server',help='Payload sender listen host, Default: 55553',default='localhost')
+    args.add_argument('-l','--listen',help='Payload listen port, Default: 1111',default=1111,type=int)
+    args.add_argument('-u','--uri',help='Metasploit RPC service uri, Default: /api/1.0/',default='/api/1.0/')
     args.add_argument('-t','--type',help='Payload Type',choices=('exe','ruby','c','dll','vbs','powershell'))
-    args.add_argument('-s','--ssl',help='Enable ssl',action="store_true",default=True)
+    args.add_argument('-s','--ssl',help='Enable ssl, Default: True',action="store_true",default=True)
     args.add_argument('-v','--versobe',help='Enable debug',action="store_true")
     parser = args.parse_args()
-    term.writeLine("[*]Server Host : {host} , Server Port : {port}".format(host=parser.server,port=parser.port), term.green)
+    term.writeLine("[*]Server Host : {host} , Server Port : {port}".format(host=parser.server,port=parser.listen), term.green)
     server = ThreadingTCPServer((parser.server,parser.listen),Metasploit_RPC.Creator(parser))
     server.serve_forever()
     
